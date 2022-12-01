23fb7641117ec9bdbd16f2b5e9778d1c5da0d4b9
Barbora Paulusová <paulusob@natur.cuni.cz>
2022-12-01 12:54:43 +0100

    pokus o rocni prumer - zatim nefunguje

--- du2.py
+++ du2.py
@@ -17,13 +17,13 @@ with open ("vstup_t.csv", encoding="utf-8", newline='') as f,\
             #print(int(row[3]))
         except ValueError:
             pass
-    print (b)
+    #print (b)
     #print (a)
     #print (len (a))
 n=len (a)
-print (n)
+#print (n)
 m=(n//7)+1
-print (m)
+#print (m)
 y=7
 #cisla=[]
 
@@ -45,3 +45,62 @@ for v in range (m):
 
 print (f"Prumerny prutok je: {prutok/len(a)}")
 
+
+with open ("vstup_t.csv", encoding="utf-8", newline='') as f,\
+    open ("vystup_7dni.csv","w",encoding="utf-8", newline='') as fout,\
+    open ("vystup_rok.csv","w",encoding="utf-8", newline='') as fout:
+    reader=csv.reader(f, delimiter=",")
+    writer = csv.writer (fout)
+    
+    #prvni_radek=f.readline(4)
+    #print (prvni_radek)
+    #datum_zac=(prvni_radek[2])
+    #rok_zac=(datum_zac[-4:])
+    #print(datum_zac)
+    #print (rok_zac)
+
+    #extrahovat datum a rok
+    
+    rok=1980
+
+    a=[]
+    b=[]
+    i=0
+    prutok=0
+    cislo=0
+    i=1
+    for row in reader:
+        
+        b.append (row)
+        datum_n=(row[2])
+        rok_n=(datum_n[-4:])
+        prutok_den=(row[-1])
+        print (rok_n)
+        if rok_n == rok:
+            a.append(row[-1])
+            cislo+=float(a.pop (0))
+            datum=(row[2])
+            rok(datum[-4:])
+            i=i+1
+        else:
+            vysledek = cislo/i
+            print (vysledek)
+            print (row[0],row[1],row[2])
+            a.clear()
+            a.append(row[-1])
+            datum=(row[2])
+            print (datum)
+            #rok(datum[-4:])
+            cislo = 0
+    
+        #rok=(datum[-4:])
+        #print(datum)
+        #print(rok)
+
+        #try:
+            #print(int(row[3]))
+        #except ValueError:
+            #pass
+    #print (b)
+    #print (a)
+    #print (len (a))
